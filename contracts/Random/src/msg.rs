87f4fb4542b1daa9026a72052e02cd39945a16b2
sinhvienit17 <nguyentuanquangsang1999@gmail.com>
2022-06-17 01:27:09 +0700

    rand

--- contracts/Random/src/msg.rs
+++ contracts/Random/src/msg.rs
@@ -1,27 +1,24 @@
+use cosmwasm_std::Addr;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
+    pub owner: Addr,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
-}
+pub enum ExecuteMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+pub enum QueryMsg<'a> {
+    Random { seed: &'a [u8], entropy: &'a [u8] },
+    GetOwner {},
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct RandResponse {
+    pub bytes: [u8; 32],
 }
