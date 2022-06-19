8e300db9e5e5cc07cdb27513ecd547ec91d07491
sinhvienit17 <nguyentuanquangsang1999@gmail.com>
2022-06-19 15:58:57 +0700

    define-agent

--- contracts/Agent/src/msg.rs
+++ contracts/Agent/src/msg.rs
@@ -1,27 +1,51 @@
+use cosmwasm_std::{Timestamp, Addr};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-
+use crate::state::{Histories,DestroyThreshold};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
+    pub rand_addr: String,
+    pub cw20_addr: String,
+    pub cw721_address: String,
+    pub start_time: Timestamp,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    Stake {amount: u128},
+    Unstake {amount: u128},
+    Claim {},
+    Withdraw {},
+    SetStartTime {new_start_time: Timestamp}
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    GetStakedInfo {account: Addr},
+    GetTimeStart {},
+    GetDestroyRate { account: Addr},
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct StakedInfoResponse {
+    pub total_staked_amount: u128,
+    pub pending_rewards: u128,
+    pub last_claim: Timestamp,
+    pub histories: Vec<Histories>,
+    pub destroy_threshold: DestroyThreshold,
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct TimeResponse {
+    pub time: Timestamp,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct RateResponse {
+    pub rate: u64,
+}
+
--- contracts/Agent/src/state.rs
+++ contracts/Agent/src/state.rs
@@ -1,13 +1,45 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, Timestamp};
+use cw_storage_plus::{Item, Map};
+
+const STATE_KEY: &str = "state";
+const STAKED_ACCOUNT_INFOS_KEY: &str = "staked_account_infos";
+const THRESH_HOLD_KEY: &str = "threshold";
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub enum DestroyThreshold {
+    LightAir,
+    StrongBreeze,
+    Hurricane,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Histories {
+    pub action: String,
+    pub amount: u128,
+    pub time: Timestamp,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct StakedAccountInfo {
+    pub total_staked_amount: u128,
+    pub pending_rewards: u128,
+    pub last_claim: Timestamp,
+    pub histories: Vec<Histories>,
+    pub destroy_threshold: DestroyThreshold,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub count: i32,
+    pub rand_addr: String,
+    pub cw20_addr: String,
+    pub cw721_address: String,
     pub owner: Addr,
+    pub start_time: Timestamp,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+pub const STATE: Item<State> = Item::new(STATE_KEY);
+pub const STAKED_ACCOUNT_INFOS: Map<&Addr, StakedAccountInfo> = Map::new(STAKED_ACCOUNT_INFOS_KEY);
+pub const THREHOLD: Map<&Addr, u64> = Map::new(THRESH_HOLD_KEY);
